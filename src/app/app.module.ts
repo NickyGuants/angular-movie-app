68422de33a79ff0fcc0cc774bd29b5289e47c4f6
NickyGuants <nicholasguantai528@gmail.com>
2022-06-08 15:41:45 +0300

    implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './components/movie/movie.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { NewReleasesComponent } from './components/new-releases/new-releases.component';
+import { UpcomingComponent } from './components/upcoming/upcoming.component';
+import { HeaderComponent } from './components/header/header.component';
 
+const appRoutes: Routes = [
+  { path: '', component: MoviesComponent },
+  { path: 'new-releases', component: NewReleasesComponent },
+  { path: 'upcoming', component: UpcomingComponent },
+  {path: 'favorites', component: FavoritesComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     MovieComponent,
-    MoviesComponent
+    MoviesComponent,
+    SidenavComponent,
+    FavoritesComponent,
+    NewReleasesComponent,
+    UpcomingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
