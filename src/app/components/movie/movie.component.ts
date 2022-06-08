68cf56cc3137f28db9c1b12a48965436c3152ad7
NickyGuants <nicholasguantai528@gmail.com>
2022-06-08 15:42:55 +0300

    get movies

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,26 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { MovieService } from 'src/app/services/movie.service';
-
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent implements OnInit {
-  
-  movie: any={}
+  @Input() movie!: any
 
-  constructor (private movieService: MovieService) { }
+
+  constructor () { }
   
 
   ngOnInit(): void {
-    this.movieService.getMovies().subscribe((movies) => movies.map((movie: any) => {
-      let movieId = movie.id;
-      let title = movieId.split("/")[2]
-      this.movieService.getMovieDetails(title).subscribe((movie) => this.movie = movie);
-    }))
-
+   
   }
-
 
 }
